6c96f2c57e2b3e61faa5831a7eb51d96645710a9
srulybrach <srulybrach@gmail.com>
2021-11-16 19:30:06 -0500

    Added in skeletons of Test methods

---
+++ lib/src/test/java/edu/touro/mco152/bm/DiskWorkerTest.java
@@ -0,0 +1,70 @@
+package edu.touro.mco152.bm;
+
+import edu.touro.mco152.bm.ui.Gui;
+import edu.touro.mco152.bm.ui.MainFrame;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiskWorkerTest {
+    /**
+     * Bruteforce setup of static classes/fields to allow DiskWorker to run.
+     *
+     * @author lcmcohen
+     */
+
+
+    private void setupDefaultAsPerProperties()
+    {
+        /// Do the minimum of what  App.init() would do to allow to run.
+        Gui.mainFrame = new MainFrame();
+        App.p = new Properties();
+        App.loadConfig();
+        System.out.println(App.getConfigString());
+        Gui.progressBar = Gui.mainFrame.getProgressBar(); //must be set or get Nullptr
+
+        // configure the embedded DB in .jDiskMark
+        System.setProperty("derby.system.home", App.APP_CACHE_DIR);
+
+        // code from startBenchmark
+        //4. create data dir reference
+        App.dataDir = new File(App.locationDir.getAbsolutePath()+File.separator+App.DATADIRNAME);
+
+        //5. remove existing test data if exist
+        if (App.dataDir.exists()) {
+            if (App.dataDir.delete()) {
+                App.msg("removed existing data dir");
+            } else {
+                App.msg("unable to remove existing data dir");
+            }
+        }
+        else
+        {
+            App.dataDir.mkdirs(); // create data dir if not already present
+        }
+    }
+
+    @Test
+    void startBenchmark(){
+        //setupDefaultAsPerProperties();
+        //App.init();
+    }
+
+    @Test
+    void accurateProgress(){
+
+    }
+
+    @Test
+    void completed(){
+
+    }
+
+    @Test
+    void actedProperly(){
+
+    }
+}
